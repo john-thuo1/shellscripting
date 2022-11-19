fd7097c9d633f50282a25fda85f7bcb6d1d37dcd
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2022-11-19 18:54:31 +0200

    Update mains.sh

--- data-shell/creatures/mains.sh
+++ data-shell/creatures/mains.sh
@@ -1,35 +1,37 @@
-#echo "Processes Running In The Computer"
-#echo
-#ps -e
-#echo
+echo "Processes Running In The Computer"
+echo
+ps -e
+echo
+#--------------------------------------------------------------------------------------------------------------------------------------
 
-#echo "Process consuming most CPU"
-#ps -eo pid,ppid,%mem,%cpu,comm --sort=-%cpu | head -n 2
+echo "Process consuming most CPU"
+ps -eo pid,ppid,%mem,%cpu,comm --sort=-%cpu | head -n 2
 
-#echo
+echo
+#--------------------------------------------------------------------------------------------------------------------------------------
 
-#echo "Process consuming most Memory"
-#ps -eo pid,ppid,%mem,%cpu,comm --sort=-%mem | head -n 2
-#echo
+echo "Process consuming most Memory"
+ps -eo pid,ppid,%mem,%cpu,comm --sort=-%mem | head -n 2
+echo
+#--------------------------------------------------------------------------------------------------------------------------------------
 
-#echo "Please enter the Process ID you want to kill : "
-#read Process_ID
-#if test -d /proc/"$Process_ID";
-#then 
-#   sudo kill -9 $Process_ID
-#   echo "Process Killed"
-#   ps -aux
-#else
-  # echo "Process ID entered does not exist"
-#fi
+echo "Please enter the Process ID you want to kill : "
+read Process_ID
+if test -d /proc/"$Process_ID";
+then 
+   sudo kill -9 $Process_ID
+   echo "Process Killed"
+   ps -aux
+else
+   echo "Process ID entered does not exist"
+fi
+echo
+#---------------------------------------------------------------------------------------------------------------------------------------
 
-#echo
 echo "File(s) that start with a number"
 echo 
 for i in [0123456789]*
-
 do
-
 if [ -f $i ]
 then
 echo $i
@@ -38,4 +40,4 @@ echo  "No such File in the directory!"
 fi
 done
 
-
+#------------------------------------------------------------------------------------------------------------------------------------------
